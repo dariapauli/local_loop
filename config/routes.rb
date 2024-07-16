1d5c39276abe73b985496cfae1021b9f049a97c9
Jay Cortes <cortesjay1991@gmail.com>
2024-07-16 15:51:54 +0200

    Destroy function is done and it works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :events, only: [:new, :create]
+  resources :events, only: [:new, :create, :show, :delete]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
