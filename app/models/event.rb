30b8bd7cf872ddd2ccf508ecae4960a0495faf86
Rahel Pauli <rahel.pauli@gmx.de>
2024-07-16 13:05:26 +0200

    added all_ages to age_group

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  enum age_group: { children: 0, teenagers: 1, young_adults: 2, middle_aged: 3, adults: 4, seniors: 5 }
+  enum age_group: { children: 0, teenagers: 1, young_adults: 2, middle_aged: 3, adults: 4, seniors: 5, all_ages: 6 }
 end
