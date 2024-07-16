d25da08f11735fbdc1b36a95458222e3adbacf77
Rahel Pauli <rahel.pauli@gmx.de>
2024-07-16 16:05:17 +0200

    implemented map

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,13 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {event: event})
+      }
+    end
   end
 
   def show
