35518ba390accb209e9340a8a0410a500994d301
Jay Cortes <cortesjay1991@gmail.com>
2024-07-16 14:33:40 +0200

    We require Show function to test the create function as we always get redirected to the element id

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,26 @@
 class EventsController < ApplicationController
 
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
     @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :address, :date, :time, :age_group)
   end
 end
