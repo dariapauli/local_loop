edffe7a6d4be370d30f2d5da5b2079f6f0c6d0cf
Jay Cortes <cortesjay1991@gmail.com>
2024-07-17 13:09:10 +0200

    events route is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :events, only: [:new, :create, :show, :delete]
+  resources :events, only: [:index, :new, :create, :show, :delete]
+  resources :pages, only: [:home]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
