a617ab36d56af020347b0dd3083123df06a60bca
Rahel Pauli <rahel.pauli@gmx.de>
2024-07-17 15:43:55 +0200

    worked on validates for event and also on the new event form for age group and category

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,11 +9,9 @@ class Event < ApplicationRecord
   # validates :description, presence: true, length: { maximum: 200 }
   validates :time, presence: true
   # validates :price, numericality: { greater_than: 0.00, message: "Price cannot be lesser than 0" }
+  CATEGORY = %w[Music Comedy Food Drinks Market Help Lesson Party Gardening Literature Sports Pets Religion Film Babies Housesitting Weird Other]
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  # def date_must_be_in_the_future
-  #   errors.add(:date, "The event date cannot be in the past. Please select a future date.") unless date.future?
-  # end
 end
