1628976c5692c62ac1aeebcb290e43b270a46737
Lukas Bertram <bertram.lukas@gmail.com>
2024-07-17 12:32:59 +0200

    Implemented participate button

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :bookings
 
   enum age_group: { children: 0, teenagers: 1, young_adults: 2, middle_aged: 3, adults: 4, seniors: 5, all_ages: 6 }
 end
