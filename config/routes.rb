06e8abfc892aa4b52a5e9e2d4b8cb73696f92dc5
sophie ranaivoharisoa <sophie.rhf@gmail.com>
2024-07-17 15:10:41 +0200

    search bar route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :events, only: [:new, :create, :show, :delete]
+  resources :events, only: [:new, :create, :show, :delete] do
+    collection do
+      get 'search'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :events
   # Defines the root path route ("/")
   # root "posts#index"
 end
