9309aa8db294c797873874f1ea051fe029a23b73
sophie ranaivoharisoa <sophie.rhf@gmail.com>
2024-07-18 17:36:25 +0200

    params city removed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   def index
     # @events = Event.all
 
-    if params[:search][:postcode].present? && params[:search][:city].present?
+    if params[:search][:postcode].present? # && params[:search][:city].present?
       @events = Event.where('address ILIKE ? OR address ILIKE ?', "%#{params[:search][:postcode]}%", "%#{params[:search][:city]}%")
     elsif params[:postcode].blank?
       flash[:alert] = "Postcode is required"
