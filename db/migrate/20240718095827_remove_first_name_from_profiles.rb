603b9dcbd6027094e444a0816ffd4e11e5d10f47
sophie ranaivoharisoa <sophie.rhf@gmail.com>
2024-07-19 18:02:28 +0200

    remove old migration files

--- db/migrate/20240718095827_remove_first_name_from_profiles.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFirstNameFromProfiles < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :profiles, :first_name, :string
-    add_column :users, :first_name, :string
-  end
-end
