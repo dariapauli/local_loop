8b1d921242a8c0ea4a59807c765e4aff96a3f92a
Jay Cortes <cortesjay1991@gmail.com>
2024-07-20 15:58:54 +0200

    Chat is working

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,6 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
 
-  validates :sender_id, presence: true
-  validates :receiver_id, presence: true
+  # validates :sender_id, presence: true
+  # validates :receiver_id, presence: true
 end
